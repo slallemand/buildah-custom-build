62cdd828cb37310e3952c0e47cd68366b91cf061
Sebastien LALLEMAND <sld@redhat.com>
2024-06-20 15:52:23 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Note that in this case the build inputs are part of the custom builder image, but normally this
 # is retrieved from an external source.
-cd /tmp/input
+# cd /tmp/input
 # OUTPUT_REGISTRY and OUTPUT_IMAGE are env variables provided by the custom
 # build framework
 TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
@@ -13,9 +13,10 @@ buildah --storage-driver vfs bud --isolation chroot -t ${TAG} .
 
 # buildah requires a slight modification to the push secret provided by the service
 # account to use it for pushing the image
-cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
-(echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
+# cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
+# (echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
 
 
 # push the new image to the target for the build
-buildah --storage-driver vfs push --tls-verify=false --authfile /tmp/.dockercfg ${TAG}
+# buildah --storage-driver vfs push --tls-verify=false --authfile /tmp/.dockercfg ${TAG}
+# buildah --storage-driver vfs push --tls-verify=false --authfile /tmp/.dockercfg ${TAG}
